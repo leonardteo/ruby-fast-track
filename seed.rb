f0c5b83cfbbf85ce2e78063280f96e1f3ef88326
Leonard Teo <leonard@ballistiq.com>
2014-02-24 11:43:19 -0500

    Put seed in modules

--- seed.rb
+++ seed.rb
@@ -1,31 +1,32 @@
 # Seeds a SQLite3 database
 require 'sqlite3'
 
-db_file = "blog.db"
+module Seed
+  def self.define_schema(db_file)
+    if File.exists? db_file
+      File.delete db_file
+    end
 
-def define_schema(db_file)
-  if File.exists? db_file
-    File.delete db_file
+    db = SQLite3::Database.new db_file
+    db.execute "CREATE TABLE users ( id INTEGER PRIMARY KEY ASC, first_name VARCHAR(255), last_name VARCHAR(255) )"
+    db.execute "CREATE TABLE posts ( id INTEGER PRIMARY KEY ASC, title VARCHAR(255), body TEXT, user_id INTEGER )"
   end
 
-  db = SQLite3::Database.new db_file
-  db.execute "CREATE TABLE users ( id INTEGER PRIMARY KEY ASC, first_name VARCHAR(255), last_name VARCHAR(255) )"
-  db.execute "CREATE TABLE posts ( id INTEGER PRIMARY KEY ASC, title VARCHAR(255), body TEXT, user_id INTEGER )"
+  def self.seed_data(db_file)
+    db = SQLite3::Database.new db_file
+
+    # Insert a user
+    db.execute "INSERT INTO users (first_name, last_name) VALUES ('Leo', 'Teo')"
+
+    # Insert posts
+    lorem_ipsum = "Bacon ipsum dolor sit amet beef shoulder frankfurter brisket short loin. Capicola shankle pork belly, turducken chuck doner leberkas short loin. Boudin strip steak pork loin shankle flank spare ribs shoulder. Ball tip leberkas beef shank jerky beef ribs tongue capicola short loin pork belly filet mignon ribeye pork doner."
+    db.execute "INSERT INTO posts (title, body, user_id) VALUES ('Hello Blog', '#{lorem_ipsum}', 1)"
+    db.execute "INSERT INTO posts (title, body, user_id) VALUES ('Hello Blog 2', '#{lorem_ipsum}', 1)"
+    db.execute "INSERT INTO posts (title, body, user_id) VALUES ('Hello Blog 3', '#{lorem_ipsum}', 1)"
+  end
 end
 
-def seed_data(db_file)
+db_file = "blog.db"
 
-  db = SQLite3::Database.new db_file
-  
-  # Insert a user
-  db.execute "INSERT INTO users (first_name, last_name) VALUES ('Leo', 'Teo')"
-
-  # Insert posts
-  lorem_ipsum = "Bacon ipsum dolor sit amet beef shoulder frankfurter brisket short loin. Capicola shankle pork belly, turducken chuck doner leberkas short loin. Boudin strip steak pork loin shankle flank spare ribs shoulder. Ball tip leberkas beef shank jerky beef ribs tongue capicola short loin pork belly filet mignon ribeye pork doner."
-  db.execute "INSERT INTO posts (title, body, user_id) VALUES ('Hello Blog', '#{lorem_ipsum}', 1)"
-  db.execute "INSERT INTO posts (title, body, user_id) VALUES ('Hello Blog 2', '#{lorem_ipsum}', 1)"
-  db.execute "INSERT INTO posts (title, body, user_id) VALUES ('Hello Blog 3', '#{lorem_ipsum}', 1)"
-end
-
-define_schema(db_file)
-seed_data(db_file)
+Seed.define_schema(db_file)
+Seed.seed_data(db_file)
